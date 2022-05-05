a1ecf2dad5a84441284c1f3972ab50c9934fe696
Peter Fiee <peterfiee@icloud.com>
2022-05-05 05:36:08 -0400

    complete outlier exercise

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -1,19 +1,71 @@
-#!/usr/bin/python3
-import os
 import joblib
+import numpy as np
+import os
 import sys
-import matplotlib.pyplot
+
 sys.path.append(os.path.abspath("../tools/"))
-from feature_format import featureFormat, targetFeatureSplit
+
+from feature_format import featureFormat
 
 
-### read in data dictionary, convert to numpy array
-data_dict = joblib.load( open("../final_project/final_project_dataset.pkl", "rb") )
+# read in data dictionary, convert to numpy array
+with open("../final_project/final_project_dataset.pkl", "rb") as file:
+    data_dict = joblib.load(file)
+    data_dict.pop("TOTAL", 0)
+
+
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
+salary = np.array(data[:, 0]).reshape(-1, 1)
+bonus = np.array(data[:, 1]).reshape(-1, 1)
 
-### your code below
+
+# Split data into train and test sets
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    salary, bonus, test_size=0.1, random_state=42
+)
+
+# Linear Regression
+from sklearn.linear_model import LinearRegression
+
+reg = LinearRegression()
+reg.fit(X_train, y_train)
+
+y_pred = reg.predict(X_test)
+
+print(f"score: {reg.score(X_test, y_test):.3f}")
 
 
+# Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+fig, ax = plt.subplots()
+
+sns.scatterplot(x=salary[:, 0], y=bonus[:, 0], ax=ax)
+sns.lineplot(x=X_test[:, 0], y=y_pred[:, 0], ax=ax, color="r")
+sns.despine()
+
+fig.suptitle("Bonus vs Salary")
+ax.set_xlabel("Salary")
+ax.set_ylabel("Bonus")
+
+plt.show()
+
+
+# find executives making more than $1e6 in salary and $5e6 in bonuses
+
+greedy = {
+    key
+    for key, val in data_dict.items()
+    if isinstance(val.get("salary"), int)
+    and val.get("salary") > 1000000
+    and val.get("bonus") > 5000000
+}
+
+print(f"\nThe following {len(greedy)} make too much")
+for name in greedy:
+    print(name)
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+import math
 
 
 def outlierCleaner(predictions, ages, net_worths):
@@ -7,14 +7,29 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
-    ### your code goes here
+    ages = ages.reshape((1, len(ages)))[0]
+    net_worths = net_worths.reshape((1, len(ages)))[0]
+    predictions = predictions.reshape((1, len(ages)))[0]
 
-    
+    '''The zip() function is used to take an iterable object as a parameter,
+    pack the corresponding elements in the object into tuples,
+    and then return a list of these tuples.'''
+
+    cleaned_data = zip(ages, net_worths, abs(net_worths-predictions))
+    # sort by error size
+    cleaned_data = sorted(cleaned_data, key=lambda x: (x[2]))
+    # ceil() function returns the integer of the number
+    # and counts the number of elements to delete
+    cleaned_num = int(-1 * math.ceil(len(cleaned_data) * 0.1))
+    # slice
+    cleaned_data = cleaned_data[:cleaned_num]
+
+    # your code goes here
+
     return cleaned_data
-
